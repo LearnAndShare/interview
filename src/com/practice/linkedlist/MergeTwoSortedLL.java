e015cade10941b6c63174e0ba313a044eccdb015
amalhotra <arti.malhotra@live.com>
2019-09-17 14:42:54 -0700

    Added priority queue sort

--- src/com/practice/linkedlist/MergeTwoSortedLL.java
+++ src/com/practice/linkedlist/MergeTwoSortedLL.java
@@ -1,4 +1,8 @@
 package com.practice.linkedlist;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /*
 https://leetcode.com/problems/merge-two-sorted-lists/description/
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
@@ -42,11 +46,41 @@ public class MergeTwoSortedLL {
         return tmp.next;
     }
 
+
+    public ListNode mergeTwoListsPQ(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>(){
+            public int compare(ListNode l1, ListNode l2){
+                return l1.val - l2.val;
+            }
+        });
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+
+        queue.offer(l1);
+        queue.offer(l2);
+
+        while(!queue.isEmpty()){
+            ListNode n = queue.poll();
+            p.next = n;
+            p=p.next;
+
+            if(n.next!=null)
+                queue.offer(n.next);
+        }
+
+        return head.next;
+    }
+
     public static void main(String[] args) {
         ListNode l1 = LinkedListUtil.getLinkedListFromString("1,2,4");
         ListNode l2 = LinkedListUtil.getLinkedListFromString("1,3,4");
         MergeTwoSortedLL ms = new MergeTwoSortedLL();
-        ListNode fl = ms.mergeTwoLists(l1,l2);
+        ListNode fl = ms.mergeTwoListsPQ(l1,l2);
         LinkedListUtil.print(fl);
     }
 }
