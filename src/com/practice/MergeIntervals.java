d7aa6b8e7e10347cbc21b460378bb3b74edea94b
amalhotra <arti.malhotra@live.com>
2020-02-07 15:59:11 -0800

    Modieifed

--- src/com/practice/MergeIntervals.java
+++ src/com/practice/MergeIntervals.java
@@ -1,4 +1,10 @@
 package com.practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /*
 https://leetcode.com/problems/merge-intervals/
 Given a collection of intervals, merge all overlapping intervals.
@@ -15,28 +21,59 @@ Output: [[1,5]]
 Explanation: Intervals [1,4] and [4,5] are considered overlapping.
  */
 public class MergeIntervals {
+    class Interval{
+        public int start;
+        public int end;
+
+        @Override
+        public String toString() {
+            return "[" + start +","+ end + "]";
+        }
+    }
+    private class IntervalComparator implements Comparator<Interval> {
+        @Override
+        public int compare(Interval a, Interval b) {
+            return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
+        }
+    }
     public int[][] merge(int[][] intervals) {
-        if(intervals == null || intervals.length ==0 )
-            return intervals;
+        List<Interval> intervalList = new ArrayList<>();
+        for(int i=0;i<intervals.length;i++) {
+            Interval inte= new Interval();
+            inte.start = intervals[i][0];
+            inte.end = intervals[i][1];
+            intervalList.add(inte);
+        }
+        Collections.sort(intervalList,new IntervalComparator());
+        System.out.println("============");
+        intervalList.forEach(System.out::println);
+        Interval prev = null;
+        List<Interval> mergedList = new ArrayList<>();
+         /*
+        Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+         */
+        for (Interval in:intervalList){
 
-        int r = intervals.length;
-        int c = intervals[0].length;
-        int [][] mergedInterval = new int[r][c];
-        int lx = intervals[0][0];
-        int ly = intervals[0][1];
-        int mi = 0;
-        for(int i=1;i<r;i++){
-            int [] d = intervals[i];
-            if(d[0] >lx && d[0] < ly && d[1] < ly){
-               ly = d[1];
-               mergedInterval[mi++] = new int[]{lx,ly};
-            } else {
-
+            if(prev == null || in.start>prev.end) {
+                mergedList.add(in);
+                prev=in;
+            }else {
+                prev.end=Math.max(prev.end,in.end);
             }
 
-        }
 
-        return mergedInterval;
+        }
+        System.out.println("===========");
+        mergedList.forEach(System.out::println);
+        int[][] intArr = new int[mergedList.size()][2];
+        int idx = 0;
+        for(Interval i:mergedList){
+            intArr[idx][0] = i.start;
+            intArr[idx][1] = i.end;
+            ++idx;
+        }
+        return intArr;
 
     }
 }
